c0e918b1ae7932ac717879e6320020531b289cab
Riz Maulana <muhamaul@gmail.com>
2016-04-18 13:27:48 +0700

    Change union() and add extract_text()
    modifikasi union dan extract_text untuk ekstrak teks dari html.

--- crawl_web.py
+++ crawl_web.py
@@ -1,8 +1,22 @@
 import urllib
+from bs4 import BeautifulSoup
 
 def get_page(url):
     page = urllib.urlopen(url).read()
     return page
+    
+def extract_text(page):
+    soup = BeautifulSoup(page)
+    for script in soup(["script", "style"]):
+        script.extract()
+    text = soup.get_text()
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    #drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    return text
 
 # adding keyword into index
 def add_to_index(index, keyword, url):
@@ -55,9 +69,7 @@ def get_next_target(page):
 
 # join two list and save it on the first list
 def union(p,q):
-    for e in q:
-        if e not in p:
-            p.append(e)
+    return list(set(p) | set(q))
 
 # get all links on a page content
 def get_all_links(page):
