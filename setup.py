c3564ae6894075a8d3570752fbc660e91e7443d5
hamayame <me.muhammad.aj@gmail.com>
2016-06-12 15:18:41 +0700

    Tambah fungsi distribusi (UNIX)

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name = "ubsearch",
+      version = "0.0.2",
+      description = "Search Engine utilities",
+      author = "ubSearch_TEAM",
+      url = "https://github.com/mrmaulana/ubsearch",
+      packages = ["src"]
+      )
